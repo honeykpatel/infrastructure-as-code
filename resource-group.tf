8f515b31b194a2c45de66a966d722b7e0c84b8ef
Darji, Mahimna Gaurang <mdarji@iu.edu>
2024-04-19 12:49:55 -0400

    Practicum-16 Changes

--- resource-group.tf
+++ resource-group.tf
@@ -1,5 +1,4 @@
-# creates a resource group in Azure
 resource "azurerm_resource_group" "practicum16" {
-  name = "practicum16"
-  location = "eastus"
+    name = var.app_name
+    location = var.location
 }
